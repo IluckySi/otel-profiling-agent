f493b20d9e786a21b46a86ea250d044b43f8a133
ilucky.si <ilucky.si@cloudwise.com>
2024-05-28 09:52:37 +0800

    IDEBUG

--- tracer/tracer.go
+++ tracer/tracer.go
@@ -448,13 +448,7 @@ func loadAllMaps(coll *cebpf.CollectionSpec, ebpfMaps map[string]*cebpf.Map) err
 	}
 
 	for mapName, mapSpec := range coll.Maps {
-		log.Errorf("Ilucky...tracer.go.loadAllMaps...mapName=%s", mapName) // Ilucky...tracer.go.loadAllMaps...mapName=v8_procs,mapSpec=Hash(keySize=4, valueSize=28, maxEntries=1024, flags=0)
-		// 获取结构体类型信息
-		t := reflect.TypeOf(mapSpec)
-		for i := 0; i < t.NumField(); i++ {
-			field := t.Field(i)
-			log.Errorf("Ilucky...tracer.go.loadAllMaps...mapName=%s, mapValue=%s", field.Name, field.Type)
-		}
+		log.Errorf("Ilucky...tracer.go.loadAllMaps...mapName=%s, name=%s, value=%v, type=%v, contents=%v", mapName, mapSpec.Name, mapSpec.Value, mapSpec.Contents) // Ilucky...tracer.go.loadAllMaps...mapName=v8_procs,mapSpec=Hash(keySize=4, valueSize=28, maxEntries=1024, flags=0)
 		if newSize, ok := adaption[mapName]; ok {
 			log.Debugf("Size of eBPF map %s: %v", mapName, newSize)
 			mapSpec.MaxEntries = newSize
